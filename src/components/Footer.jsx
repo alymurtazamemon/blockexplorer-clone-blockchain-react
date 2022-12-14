7a2bafb459cc816412f3223b331e6ec027ee68df
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-15 00:16:20 +0500

    design the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,36 @@
+import {
+    AiFillTwitterSquare,
+    AiFillLinkedin,
+    AiFillGithub,
+} from "react-icons/ai";
+
 function Footer() {
-    return <footer>Footer</footer>;
+    return (
+        <footer className="flex px-24 py-6 bg-sky-50 text-center items-center">
+            <p className="mr-auto text-lg">© 2022 Ali Murtaza</p>
+            <div className="flex items-center w-1/5 justify-between">
+                <p className="text-xl">Follow Me</p>
+                <a
+                    href="https://www.twitter.com/alymurtazamemon"
+                    target="_blank"
+                >
+                    <AiFillTwitterSquare size="36" color="#00acee" />
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/alymurtazamemon"
+                    target="_blank"
+                >
+                    <AiFillLinkedin size="36" color="#0A66C2" />
+                </a>
+                <a
+                    href="https://www.github.com/alymurtazamemon"
+                    target="_blank"
+                >
+                    <AiFillGithub size="36" color="#171515" />
+                </a>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
