4bae1db6b1c3dd88cb7c8faab33a3ef94804d5e0
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-14 11:52:26 +0500

    update block

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,50 +1,39 @@
-import alchemy from "../utils/alchemy";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 
+import { DataContext } from "../context/DataContext";
+
 function Block() {
-    const [blockInfo, setBlockInfo] = useState(<div>Loading...</div>);
+    const { blocksData } = useContext(DataContext);
     const { blockNumber } = useParams();
+    const info = blocksData.find(
+        (blockData) => blockData.number == blockNumber
+    );
 
-    useEffect(() => {
-        async function getBlockInformation() {
-            const info = await alchemy.core.getBlockWithTransactions(
-                Number(blockNumber)
-            );
-            console.log(info);
-            setBlockInfo(
-                <section>
-                    <p>Block {info.number}</p>
-                    <p>Time Stamp {info.timestamp}</p>
-                    <p>
-                        Transactions{" "}
-                        <Link to={`/txs?block=${info.number}`}>
-                            {info.transactions != undefined
-                                ? info.transactions.length
-                                : 0}
-                            {" transactions"}
-                        </Link>
-                    </p>
-                    <p>Fee Recipient {info.miner}</p>
-                    <p>Gas Used {info.gasUsed.toString()}</p>
-                    <p>Gas Limit {info.gasLimit.toString()}</p>
-                    <p>
-                        Base Fee Per Gas{" "}
-                        {info.baseFeePerGas.toString() / 1000000000} Gwei
-                    </p>
-                    <p>Extra Data {info.extraData}</p>
-                    <p>Hash {info.hash}</p>
-                    <p>Parent Hash {info.hash}</p>
-                    <p>Nonce {info.nonce}</p>
-                    <p>Difficulty {info.difficulty}</p>
-                </section>
-            );
-        }
-
-        getBlockInformation();
-    }, []);
-
-    return blockInfo;
+    return (
+        <section>
+            <p>Block {info.number}</p>
+            <p>Time Stamp {info.timestamp}</p>
+            <p>
+                Transactions
+                <Link to={`/txs?block=${info.number}`}>
+                    {info.transactions != undefined
+                        ? info.transactions.length
+                        : 0}
+                    {" transactions"}
+                </Link>
+            </p>
+            <p>Fee Recipient {info.miner}</p>
+            <p>Gas Used {info.gasUsed}</p>
+            <p>Gas Limit {info.gasLimit}</p>
+            <p>Base Fee Per Gas {info.baseFeeInGwei} Gwei</p>
+            <p>Extra Data {info.extraData}</p>
+            <p>Hash {info.hash}</p>
+            <p>Parent Hash {info.hash}</p>
+            <p>Nonce {info.nonce}</p>
+            <p>Difficulty {info.difficulty}</p>
+        </section>
+    );
 }
 
 export default Block;
