c683a23344567324a03c46662a3df92b76461d60
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-16 22:17:34 +0500

    update header titles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
 
+import ETHERSCAN_LOGO from "../assets/images/etherscan-logo-circle.png";
+
 function Header() {
     return (
-        <header className="flex px-24 py-6 bg-sky-50">
-            <h1>Etherscan Clone</h1>
-            <Link to="/" className="ml-auto">HOME</Link>
+        <header className="flex px-24 py-6 bg-sky-50 items-center">
+            <img src={ETHERSCAN_LOGO} className="h-10" />
+            <h1 className="ml-4 text-[#21325B] font-bold text-2xl">
+                Etherscan Clone
+            </h1>
+            <Link to="/" className="ml-auto text-[#21325B] font-bold text-xl">
+                HOME
+            </Link>
         </header>
     );
 }
