d4adcfe05c1f2de1f11235588329bd1ac1b50e56
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-17 00:04:58 +0500

    add route to etherscan logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,14 @@ import ETHERSCAN_LOGO from "../assets/images/etherscan-logo-circle.png";
 function Header() {
     return (
         <header className="flex px-24 py-6 bg-white items-center border-b">
-            <img src={ETHERSCAN_LOGO} className="h-10" />
-            <h1 className="ml-4 text-[#21325B] font-bold text-2xl">
-                Etherscan Clone
-            </h1>
+            <Link to="/">
+                <img src={ETHERSCAN_LOGO} className="h-10"></img>
+            </Link>
+            <Link to="/">
+                <h1 className="ml-4 text-[#21325B] font-bold text-2xl">
+                    Etherscan Clone
+                </h1>
+            </Link>
             <Link to="/" className="ml-auto text-[#21325B] font-bold text-xl">
                 HOME
             </Link>
