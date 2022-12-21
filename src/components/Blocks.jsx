2b875dabca2916e07e4f12ae19d05edd867877c0
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-21 08:19:18 +0500

    update design blocks screen

--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -6,9 +6,9 @@ import { Link } from "react-router-dom";
 function Blocks() {
     const { blocksData } = useContext(DataContext);
 
-    const blocks = blocksData.map((blockData) => {
+    const blocks = blocksData.map((blockData, index) => {
         return (
-            <div className="flex py-4">
+            <div key={index} className="flex py-4">
                 <p className="w-2/12 text-[#357BAD]">
                     <Link to={`/block/${blockData.number}`}>
                         {blockData.number}
@@ -32,18 +32,22 @@ function Blocks() {
 
     return (
         <section className="bg-white mx-24 px-8 py-4 my-8 border rounded-lg divide-y">
-            <div className="flex py-4">
-                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Block</p>
-                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Age</p>
-                <p className="w-1/12 pb-3 text-[#6C757E] font-bold">Txn</p>
-                <p className="w-3/12 pb-3 text-[#6C757E] font-bold">
+            <p className="pb-4 text-sm text-[#6C757E]">
+                Block #{blocksData[blocksData.length - 1].number} to #
+                {blocksData[0].number} (Total of {blocksData.length} Blocks)
+            </p>
+            <div className="flex py-4 bg-sky-50">
+                <p className="w-2/12 py-3 text-[#6C757E] font-bold">Block</p>
+                <p className="w-2/12 py-3 text-[#6C757E] font-bold">Age</p>
+                <p className="w-1/12 py-3 text-[#6C757E] font-bold">Txn</p>
+                <p className="w-3/12 py-3 text-[#6C757E] font-bold">
                     Fee Reciptant
                 </p>
-                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Gas Used</p>
-                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">
+                <p className="w-2/12 py-3 text-[#6C757E] font-bold">Gas Used</p>
+                <p className="w-2/12 py-3 text-[#6C757E] font-bold">
                     Gas Limit
                 </p>
-                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Base Fee</p>
+                <p className="w-2/12 py-3 text-[#6C757E] font-bold">Base Fee</p>
             </div>
             {blocks}
         </section>
