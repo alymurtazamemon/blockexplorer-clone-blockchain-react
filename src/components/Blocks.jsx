1d1abe076424e8986b7de103aee5bd427a7ce4cc
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-21 08:00:20 +0500

    design blocks screens

--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -1,23 +1,53 @@
 import { useContext } from "react";
 
 import { DataContext } from "../context/DataContext";
+import { Link } from "react-router-dom";
 
 function Blocks() {
     const { blocksData } = useContext(DataContext);
 
-    return blocksData.map((blockData) => {
+    const blocks = blocksData.map((blockData) => {
         return (
-            <section className="flex">
-                <p>{blockData.number}</p>
-                <p>{blockData.timestamp}</p>
-                <p>{blockData.transactions.length}</p>
-                <p>{blockData.miner.slice(0, 16)}...</p>
-                <p>{blockData.gasUsed}</p>
-                <p>{blockData.gasLimit}</p>
-                <p>{blockData.baseFeeInGwei} Gwei</p>
-            </section>
+            <div className="flex py-4">
+                <p className="w-2/12 text-[#357BAD]">
+                    <Link to={`/block/${blockData.number}`}>
+                        {blockData.number}
+                    </Link>
+                </p>
+                <p className="w-2/12">{blockData.timestamp}</p>
+                <p className="w-1/12 text-[#357BAD]">
+                    <Link to={`/txs?block=${blockData.number}`}>
+                        {blockData.transactions.length}
+                    </Link>
+                </p>
+                <p className="w-3/12 text-[#357BAD]">
+                    {blockData.miner.slice(0, 16)}...
+                </p>
+                <p className="w-2/12">{blockData.gasUsed}</p>
+                <p className="w-2/12">{blockData.gasLimit}</p>
+                <p className="w-2/12">{blockData.baseFeeInGwei} Gwei</p>
+            </div>
         );
     });
+
+    return (
+        <section className="bg-white mx-24 px-8 py-4 my-8 border rounded-lg divide-y">
+            <div className="flex py-4">
+                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Block</p>
+                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Age</p>
+                <p className="w-1/12 pb-3 text-[#6C757E] font-bold">Txn</p>
+                <p className="w-3/12 pb-3 text-[#6C757E] font-bold">
+                    Fee Reciptant
+                </p>
+                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Gas Used</p>
+                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">
+                    Gas Limit
+                </p>
+                <p className="w-2/12 pb-3 text-[#6C757E] font-bold">Base Fee</p>
+            </div>
+            {blocks}
+        </section>
+    );
 }
 
 export default Blocks;
